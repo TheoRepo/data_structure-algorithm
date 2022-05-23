29a7c5949473c98c9a06e9020a1a656183ca4146
Theo Yu <ty364@cornell.edu>
2022-05-23 10:41:23 +0800

    完成堆排

--- sort.py
+++ sort.py
@@ -10,15 +10,6 @@ def bubble_sort(li):
                 li[j] , li[j+1] = li[j+1], li[j]
 
 
-def select_sort_simple(li):
-    li_new = []
-    for i in range(len(li)):
-        min_val = min(li)
-        li_new.append(min_val)
-        li.remove(min_val)
-    return li_new
-
-
 # 选择排序
 # 算法复杂度O(n^2)
 def select_sort(li):
@@ -62,11 +53,11 @@ def partition(li, left, right):
         while left < right and li[right] >= tmp: # 从右边找比tmp小的数
             right -= 1 # 往左边走一步
         li[left] = li[right] # 把右边的值写到左边空位上
-        # print(li,'right')
+        print(li,'right')
         while left < right and li[left] <= tmp:
             left += 1
         li[right] = li[left] # 把左边的值写道右边空位上
-        # print(li,'left')
+        print(li,'left')
     li[left] = tmp # 把tmp归位
     return left
 
@@ -77,14 +68,64 @@ def quick_sort(li,left, right):
         quick_sort(li,mid+1, right)
 
 
+# 堆排序
+# 堆排序过程
+# 1.建立堆。
+# 2.得到堆顶元素，为最大元素。
+# 3.去掉堆顶，将堆最后一个元素放到堆顶，此时可通过一次调整重新使堆有序。
+# 4.堆顶元素为第二大元素。
+# 5.重复步骤3，直到堆变空。
+
+# 为了节省存储空间
+# 拿下来的最大元素，放在列表的尾端
+# 时间复杂度O(nlog(n))
+
+# 堆的向下调整过程
+# 当根节点的左右子树都是堆时，可以通过一次向下的调整来将其变换成一个堆
+def sift(heap, low ,high):
+    # hight是堆的最后一个元素的下标，low是第一个元素
+    i = low # i开始时父亲节点
+    j = 2*i + 1 # j开始是左孩子
+    tmp = li[low] # 把堆顶存起来
+    while j <= high: # 只要j位置有节点
+        if j + 1 <= high and li[j + 1] > li[j]: # 如果右孩子有，并且比较大
+            j = j + 1 # j指向右孩子
+        if li[j] > tmp:
+            li[i] = li[j]
+            i = j # 往下看一层
+            j = 2 * i + 1
+        else: # tmp更大，把tmp放到i的位置上
+            # li[i] = tmp # 把tmp放到某一级领导的位置 # 重复代码，注释掉
+            break
+    else: # j位置没有节点，跳出循环
+        li[i] = tmp
+
+
+# 构造堆
+def heap_sort(li):
+    n = len(li)
+    for i in range((n-2)//2, -1 , -1): # -1表示倒叙，步长是-1
+        # i表示建堆的时候调整的部分的根的下标
+        sift(li, i, n-1)
+    # 建堆完成了（农村包围城市）
+    for i in range(n-1, -1, -1): 
+        # i指向当前堆的最后一个元素
+        li[0], li[i] = li[i],li[0]
+        sift(li, 0, i-1) # i-1是新的high
+
+
 if __name__ == "__main__":
-    # li = [random.randint(0, 10000) for i in range(1000)]
-    # li_copy = copy.deepcopy(li)
-    # bubble_sort(li)
-    # select_sort(li_copy)
-    # print(li)
-    # print(li_copy)
-    # assert li == li_copy
     li = [5,7,4,6,3,1,2,9,8]
-    quick_sort(li,0,len(li)-1)
+    # li = [random.randint(0,1000) for i in range(1000)]
+    print(li)
+    # li_copy = copy.deepcopy(li)
+    # print(li_copy)
+    # 冒泡排序
+    # bubble_sort(li)
+    # 选择排序
+    # select_sort(li_copy)
+    # 快速排序
+    # quick_sort(li,0,len(li)-1)
+    # 堆排序例子
+    heap_sort(li)
     print(li)
