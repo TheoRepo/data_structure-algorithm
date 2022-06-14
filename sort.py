49acc655345d33cdf423cedc81a826b0f533b23b
Theo Yu <ty364@cornell.edu>
2022-06-15 00:41:20 +0800

    完成基数排序算法

--- sort.py
+++ sort.py
@@ -327,6 +327,33 @@ def bucket_sort(li, n=100, max_num=10000):
     return li
 
 
+# 基数排序
+# 对关键字排序：假如现在有一个员工表，要求按照薪资排序，年龄相同的员工按照年龄排序
+# 先按照年龄进行排序，再按照薪资进行稳定的排序
+# 对32,13,94,52,17,54,93排序，可以看做多关键字排序
+# 先按照个位数排序，再按照十位数分桶，最后挨个输出
+# 时间复杂度：O(kn)
+# 空间复杂度：O(k+n)
+# k表示数字位数
+def radix_sort(li):
+    max_num = max(li) # 最大值 9->1, 99->2, 888->3, 10000->5
+    it = 0
+    while 10 ** it <= max_num:
+        buckets = [[] for _ in range(10)]
+        for var in li:
+            # 987 
+            # it=1 987//10 -> 98  98%10->8
+            # it=2 987//100 -> 9 9%10 -> 9
+            digit = (var // 10 ** it) % 10
+            buckets[digit].append(var)
+        # 分桶完成
+        li.clear()
+        for buc in buckets:
+            li.extend(buc)
+        # 把数重新写回li
+        it += 1
+
+
 if __name__ == "__main__":
     # li = [5,7,4,6,3,1,2,9,8]
     # li = [random.randint(0,1000) for i in range(1000)]
@@ -341,6 +368,7 @@ if __name__ == "__main__":
     li5 = copy.deepcopy(li)
     li6 = copy.deepcopy(li)
     li7 = copy.deepcopy(li)
+    li8 = copy.deepcopy(li)
 
     li_limit1 = [random.randint(0,10) for _ in range(100)]
     li_limit2 = [random.randint(0,10) for _ in range(100)]
@@ -389,3 +417,8 @@ if __name__ == "__main__":
     print("桶排序")
     bucket_sort(li_limit2, n=2, max_num=10)
     print(li_limit2)
+
+    # 基数排序
+    print("基数排序")
+    radix_sort(li8)
+    print(li8)
