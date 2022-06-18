f4706ed1959d82dafeb7c86e9b3bd5eaf284d421
Theo Yu <ty364@cornell.edu>
2022-06-18 20:05:13 +0800

    完成栈解决括号匹配问题

--- data_structure.py
+++ data_structure.py
@@ -52,10 +52,42 @@ class Stack:
             return self.stack[-1]
         else:
             return None
-        
+
+    def is_empty(self):
+        return len(self.stack) == 0
+
+# 栈的应用-括号匹配问题
+# 括号匹配问题：给一个字符串，其中包好小括号、中括号、大括号，求改字符串中的括号是否匹配
+# 例如
+# ()()[]{}  匹配
+# ([{()}])  匹配
+# [](       不匹配
+# [(])      不匹配
+def brace_match(s):
+    match = {'}':'{', ']':'[', ')':'('}
+    stack = Stack()
+    for ch in s:
+        if ch in {'(','[','{'}:
+            stack.push(ch)
+        else: # ch in  {')',']','}'}
+            if stack.is_empty():
+                return False
+            elif stack.get_top() == match[ch]:
+                stack.pop()
+            else: # stack.get_top() != match[ch]
+                return False
+    if stack.is_empty():
+        return True
+    else:
+        return False
+
+
 if __name__ == "__main__":
     stack = Stack()
     stack.push(1)
     stack.push(2)
     stack.push(3)
     print(stack.pop())
+
+    print(brace_match('[{()}(){()}[]({}){}]'))
+    print(brace_match('[{]'))
