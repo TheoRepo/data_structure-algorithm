24e602f57df423e6eec86bfb343c983d09490880
Theo Yu <ty364@cornell.edu>
2022-06-21 00:29:57 +0800

    完成迷宫问题的学习

--- data_structure.py
+++ data_structure.py
@@ -150,6 +150,57 @@ def tail(n):
         a= deque(f, n)
         return a
 
+# 栈和队列的应用：迷宫问题
+# 给一个二维列表，表示迷宫(0表示通道，1表示围墙)。给出算法，求一条走出迷宫的路径
+# 回溯法
+# 思路：从一个节点开始，任意找下一个能走的点，当找不到能走的点时，退回上一个点寻找是否有其他方向的点。
+# 使用栈存储当前路径
+maze = [
+    [1,1,1,1,1,1,1,1,1,1],
+    [1,0,0,1,0,0,0,1,0,1],
+    [1,0,0,1,0,0,0,1,0,1],
+    [1,0,0,0,0,1,1,0,0,1],
+    [1,0,1,1,1,0,0,0,0,1],
+    [1,0,0,0,1,0,0,0,0,1],
+    [1,0,1,0,0,0,1,0,0,1],
+    [1,0,1,1,1,0,1,1,0,1],
+    [1,1,0,0,0,0,0,0,0,1],
+    [1,1,1,1,1,1,1,1,1,1]
+]
+dirs = [
+    lambda x,y: (x+1,y),
+    lambda x,y: (x-1,y),
+    lambda x,y: (x,y-1),
+    lambda x,y: (x,y+1)
+]
+
+def maze_path(x1,y1,x2,y2):
+    stack = []
+    stack.append((x1, y1))
+    while(len(stack)>0):
+        curNode = stack[-1] # 当前的节点
+        if curNode[0] == x2 and curNode[1] == y2:
+            # 走到重点了
+            for p in stack:
+                print(p)
+            return True
+        
+        # x,y四个方向，x-1,y; x+1,y ;x,y-1; x,y+1
+        for dir in dirs:
+            nextNode = dir(curNode[0], curNode[1])
+            # 如果下一个节点能走
+            if maze[nextNode[0]][nextNode[1]] == 0:
+                stack.append(nextNode)
+                maze[nextNode[0]][nextNode[1]] = 2 # 表示为已经走过
+                break
+        else:
+            maze[nextNode[0]][nextNode[1]] = 2
+            stack.pop()
+    else:
+        print("没有路")
+        return False
+
+
 if __name__ == "__main__":
 
     print("测试: 栈")
@@ -181,4 +232,7 @@ if __name__ == "__main__":
 
     print("测试: 使用python队列读取文件末尾n行的内容")
     for line in tail(5):
-        print(line, end='')    
+        print(line, end='')
+        
+    print("测试: 迷宫问题")
+    maze_path(1,1,8,8)
