0341c972c0f7d9e7428bba991f5d8abb854d1ba8
Theo Yu <leoyutian@yeah.net>
2022-08-22 12:27:18 +0800

    完成二叉搜索树的插入，寻找，删除操作的学习

--- data_structure/binary_search_tree.py
+++ data_structure/binary_search_tree.py
@@ -6,8 +6,7 @@
 
 
 # 二叉树的常规操作
-from dataclasses import dataclass
-
+import random
 
 class BiTreeNode:
     def __init__(self,data):
@@ -56,6 +55,92 @@ class BST:
                     p.rchild.parent = p
             else:
                 return
+    # 递归
+    def query(self,node, val):
+        if not node:
+            return None
+        if node.data < val:
+            # 向右边找
+            return self.query(node.rchild, val)
+        elif node.data > val:
+            # 向左边找
+            return self.query(node.lchild, val)
+        else:
+            return node
+
+    # 非递归
+    def query_no_rec(self, val):
+        p = self.root
+        while p:
+            if p.data < val:
+                p = p.rchild
+            elif p.data > val:
+                p = p.lchild
+            else:
+                return p
+        return None
+
+    def __remove_node_1(self, node):
+        # 情况1：node是叶子节点
+        if not node.parent:
+            self.node = None
+        # 把它和它父亲脱离关系
+        if node == node.parent.lchild: # node是它父亲的左孩子
+            self.parent.lchild = None
+        else: # node是它父亲的右孩子
+            node.parent.rchild = None
+            
+    def __remove_node_21(self, node):
+        # 情况2：node只有一个左孩子
+        if not node.parent: # 根节点
+            self.root = node.lchild
+            node.lchild.parent = None
+        elif node == node.parent.lchild: # node是它父亲的左孩子
+            node.parent.lchild = node.lchild 
+            node.lchild.parent = node.parent
+        else: # node是它父亲的右孩子
+            node.parent.rchild = node.lchild
+            node.lchild.parent = node.parent
+
+    def __remove_node_22(self, node):
+        # 情况2.2：只有一个右孩子
+        if not node.parent: # 根节点
+            self.root = node.rchild
+        elif node == node.parent.lchild: # node是它父亲的左孩子   
+            node.parent.lchild = node.rchild
+            node.rchild.parent = node.parent
+        else: # node是它父亲的右孩子
+            node.parent.rchild = node.rchild
+            node.rchild.parent = node.parent    
+
+    def delete(self, val):
+        if self.root: # 不是空树
+            # 先找到节点
+            node = self.query_no_rec(val)
+            if not node: # 不存在
+                return False 
+            # 第一种情况：node是叶子节点
+            if not node.lchild and not node.rchild:
+                self.__remove_node_1(node)
+            # 第二种情况：只有一个左孩子
+            elif not node.rchild: # 只有一个左孩子
+                self.__remove_node_21(node)
+            elif not node.lchild: # 只有一个右孩子
+                self.__remove_node_22(node)
+            # 第三种情况：有两个孩子
+            else:
+                #先找右子树最小的节点
+                min_node= node.rchild
+                while min_node.lchild:
+                    min_node = min_node.lchild
+                node.data = min_node.data
+                # 删除min_node
+                # 因为前一步通过递归找左孩子，所以不存在左孩子，那就只剩下两种情况，有一个右孩子，叶子节点
+                if min_node.rchild:
+                    self.__remove_node_22(min_node) 
+                else: 
+                    self.__remove_node_1(min_node)
+
 
     def pre_order(self, root):
         if root:
@@ -77,10 +162,27 @@ class BST:
             self.post_order(root.lchild)
             self.post_order(root.rchild)
             print(root.data, end=',')
-        
+
+
+# 插入功能测试   
 tree = BST([4,6,7,9,2,1,3,5,8])
 tree.pre_order(tree.root)
 print("")
-tree.in_order(tree.root)
+tree.in_order(tree.root) # 中序排列输出的是有序数列
 print("")
 tree.post_order(tree.root)
+
+# 查询功能测试
+li = list(range(0,500,2))
+random.shuffle(li)
+print("")
+tree = BST(li)
+print(tree.query_no_rec(4).data)
+
+# 删除功能测试
+tree = BST([1,4,2,5,3,8,6,9,7])
+tree.in_order(tree.root)
+print("")
+tree.delete(4)
+tree.delete(1)
+tree.in_order(tree.root)
